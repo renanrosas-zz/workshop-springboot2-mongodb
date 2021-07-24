1011d00f9b062b80dd6a78f099a30b643730983b
Renan Rosas <renan-rosas@hotmail.com>
2021-07-24 19:55:55 -0300

    Getting a user by id

--- src/main/java/com/renanrosas/workshopmongo/services/UserService.java
+++ src/main/java/com/renanrosas/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.renanrosas.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.renanrosas.workshopmongo.domain.User;
 import com.renanrosas.workshopmongo.repositories.UserRepository;
+import com.renanrosas.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
