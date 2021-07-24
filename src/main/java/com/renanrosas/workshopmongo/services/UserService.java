8e711cf255138c702b5a05ba27d7ed35c26bb280
Renan Rosas <renan-rosas@hotmail.com>
2021-07-24 20:12:18 -0300

    User insert with POST

--- src/main/java/com/renanrosas/workshopmongo/services/UserService.java
+++ src/main/java/com/renanrosas/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.renanrosas.workshopmongo.domain.User;
+import com.renanrosas.workshopmongo.dto.UserDTO;
 import com.renanrosas.workshopmongo.repositories.UserRepository;
 import com.renanrosas.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
