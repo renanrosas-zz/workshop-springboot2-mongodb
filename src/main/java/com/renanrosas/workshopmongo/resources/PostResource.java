cf29daa18438420de6c79245c24ce6032107a330
Renan Rosas <renan-rosas@hotmail.com>
2021-07-25 10:16:08 -0300

    Query methods

--- src/main/java/com/renanrosas/workshopmongo/resources/PostResource.java
+++ src/main/java/com/renanrosas/workshopmongo/resources/PostResource.java
@@ -1,12 +1,16 @@
 package com.renanrosas.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.renanrosas.workshopmongo.domain.Post;
+import com.renanrosas.workshopmongo.resources.util.URL;
 import com.renanrosas.workshopmongo.services.PostService;
 
 @RestController
@@ -22,4 +26,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
